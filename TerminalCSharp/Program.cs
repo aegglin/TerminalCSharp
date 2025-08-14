55fbe98d0ca1a08f27d126b3687f723aaadbd278
Aiden Egglin <uefadude@gmail.com>
2025-08-14 06:16:29 -0400

    First commit

---
+++ TerminalCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+
+    public static void ReadDirectory(string path)
+    {
+        DirectoryInfo di = new DirectoryInfo(path);
+        Console.WriteLine("The path in ReadDirectory is: ", path);
+        try
+        {
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                Console.WriteLine($"Name is: {fi.Name}, full name is: {fi.FullName}");
+                if (File.Exists(path))
+                {
+                    System.Console.WriteLine($"{fi.FullName} is a file, not a directory");
+                }
+                else if (Directory.Exists(path))
+                {
+                    System.Console.WriteLine($"{fi.FullName} is a directory, not a file");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Couldn't find {path}");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string path = Path.Join(@"C:\", "Users", "Aiden", "dev");
+        Console.WriteLine(path);
+        ReadDirectory(path);
+    }
+}
